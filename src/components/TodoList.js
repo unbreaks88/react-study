d6d3d28a89fca1f74734567a77da2b7e41c3aed0
unbreaks88 <unbreaks88@gmail.com>
2020-07-22 23:03:22 +0900

    일정 관리 기본 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './TodoList.scss';
-import { TodoListItem } from './TodoListItem';
+import {TodoListItem} from './TodoListItem';
 
-export const TodoList = ({ todos }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} />
-      ))}
-    </div>
-  );
+export const TodoList = ({todos, onRemove, onToggle}) => {
+    return (
+        <div className="TodoList">
+            {todos.map((todo) => (
+                <TodoListItem todo={todo} onRemove={onRemove} onToggle={onToggle} key={todo.id}/>
+            ))}
+        </div>
+    );
 };
