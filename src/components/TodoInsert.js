d68131da2ef8d9779f7edca1b51908253ca29e6d
unbreaks88 <unbreaks88@gmail.com>
2020-07-23 23:15:10 +0900

    react-virtualized를 사용한 렌더링 최적화 적용 스크롤에 보이지 않는 컴포넌트는 렌더링하지 않고 크기 만큼만 차지하도록 구현

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,30 +1,30 @@
 import React, {useCallback, useState} from "react";
-import { MdAdd } from "react-icons/md";
+import {MdAdd} from "react-icons/md";
 import "./TodoInsert.scss";
 
-export const TodoInsert = ({ onInsert }) => {
-  const [value, setValue] = useState("")
+export const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState("")
 
-  const onChange = useCallback((e) => {
-    setValue(e.target.value);
-  }, []);
+    const onChange = useCallback((e) => {
+        setValue(e.target.value);
+    }, []);
 
-  const onSubmit = useCallback(e=> {
-    onInsert(value)
-    setValue('');
-    e.preventDefault();
-  }, [onInsert, value])
+    const onSubmit = useCallback(e => {
+        onInsert(value)
+        setValue('');
+        e.preventDefault();
+    }, [onInsert, value])
 
-  return (
-    <form className="TodoInsert" onSubmit={onSubmit}>
-      <input
-        placeholder="할 일을 입력하세요"
-        value={value}
-        onChange={onChange}
-      />
-      <button type="submit">
-        <MdAdd />
-      </button>
-    </form>
-  );
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input
+                placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
+            <button type="submit">
+                <MdAdd/>
+            </button>
+        </form>
+    );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import './TodoList.scss';
+import {List} from 'react-virtualized'
 import TodoListItem from "./TodoListItem";
 
 const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRenderer = useCallback(
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem
+                    todo={todo}
+                    key={key}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                    style={style}/>
+            )
+        }, [onRemove, onToggle, todos]
+    )
     return (
-        <div className="TodoList">
-            {todos.map((todo) => (
-                <TodoListItem todo={todo} onRemove={onRemove} onToggle={onToggle} key={todo.id}/>
-            ))}
-        </div>
-    );
-};
+        <List
+            className={"TodoList"}
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{outline: 'none'}}
+        />
+    )
+}
 
 export default React.memo(TodoList)
