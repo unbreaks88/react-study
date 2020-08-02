9215d40e0e949404b60ce9e49b5d7c6cea2ed42a
unbreaks88 <unbreaks88@gmail.com>
2020-08-02 23:35:52 +0900

    News 뷰어 UI 구현 - axios로 데이터 호출 - 카테고리, 뉴스 리스트

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import styled from 'styled-components';
+
+// const NewsListBlock =
+
+// const sampleArticle = {
+//   title: '제목',
+//   description: '내용',
+//   url: 'https://www.google.com',
+//   urlToImage: 'https://via.placeholder.com/160',
+// };
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = ({ category }) => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=de963c00732342aaa7ab9bde2613c772`,
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  if (loading) {
+    return <div>대기 중...</div>;
+  }
+
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
